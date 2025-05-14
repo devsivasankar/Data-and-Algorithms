9bc1c229c845c09dde0bafe7e06555f22188549a
devsivasankar <mypostbox.sivasankar@gmail.com>
2025-05-14 23:25:02 +0530

    Update DSA.py

--- DSA.py
+++ DSA.py
@@ -1,5 +1,5 @@
 # linear search in array
-arr = [1,2,3,4,5,6,7,8,9,10]
+arr1 = [1,2,3,4,5,6,7,8,9,10]
 
 def linear_search(arr,target):
     for i in range(len(arr)):
@@ -7,5 +7,5 @@ def linear_search(arr,target):
             return i
     return 'not available'
 
-x = linear_search(arr,1)
+x = linear_search(arr1,1)
 print(x)
