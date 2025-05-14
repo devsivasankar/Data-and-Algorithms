832d5b7d22a20f93b9fc88d05423ab4056ba2c04
devsivasankar <mypostbox.sivasankar@gmail.com>
2025-05-14 23:13:48 +0530

    linear

--- DSA.py
+++ DSA.py
@@ -1,1 +1,11 @@
 # linear search in array
+arr = [1,2,3,4,5,6,7,8,9,10]
+
+def linear_search(arr,target):
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
+    return 'not available'
+
+x = linear_search(arr,5)
+print(x)
